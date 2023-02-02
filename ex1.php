a49b76a4eae376ec897a25b49fef7c5901fc7b17
thanh22000 <onnellinentc@gmail.com>
2023-02-02 21:44:02 +0200

    ex1

---
+++ ex1.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My first php page</title>
+</head>
+<body>
+    <h1> 3.1 Write a simple PHP script to print your information </h1>
+    <?php
+    echo "Thanh Trinh <br>";
+    echo "BBCAP22";
+    ?>
+
+    <h1> 3.2 Write PHP code to display the following message </h1>
+    <?php
+    echo "Hello World! My name is \"Thanh\" aka \"Luke\"! <br>";
+    echo "BBCAP22";
+    ?>
+
+    <h1> 3.3 Write the PHP code in to display the current date </h1>
+    <?php 
+    echo date("d.m.y") 
+    ?>
+
+    <h1> $title = "PHP is interesting” </h1>
+    
+</body>
+</html>
