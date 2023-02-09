1a4c8b76db91a585ba6b9e7ab4453796ecb3d429
thanh22000 <onnellinentc@gmail.com>
2023-02-09 10:06:37 +0200

    Feb 9th

---
+++ array_exercise21.php
@@ -0,0 +1,45 @@
+<?php 
+$title="Exercise 3";
+include "header.php" ?>
+
+<form action="" method="get">
+    <input type="number" placeholder="Enter your first number" name="num1" required>
+    <input type="number" placeholder="Enter your second number" name="num2" required>
+    <select name="operator">
+        <option value="add"> Add </option>
+        <option value="sub"> Subtract </option>
+        <option value="mul"> Multiply </option>
+        <option value="div"> Divide </option>
+    </select>
+    <input type="submit" name="calculate" value="Calculate">
+</form>
+<?php
+if (isset($_get["cal"])){
+    $num1=$_get["num1"];
+    $num2=$_get["num2"];
+    $operator=$_get["operator"];
+    switch($operator){
+        case "add":
+            $result=$num1+$num2;
+            break;
+        case "sub":
+            $result=$num1-$num2;
+            break;
+        case "mul":
+            $result=$num1*$num2;
+            break;
+        case "div":
+            $result=$num1/$num2;
+            break;
+        default:
+            $result="Error: You have not selected the correct operator";
+    }
+}
+if (isset($result)){
+    echo "<h2> Result: $result </h2>";
+}
+?>
+
+
+
+<?php include "footer.php" ?>
