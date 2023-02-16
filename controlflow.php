31bf75eb5a110b406cbac552a657eaa8d90fa07f
thanh22000 <onnellinentc@gmail.com>
2023-02-16 13:23:05 +0200

    Update controlflow.php

--- controlflow.php
+++ controlflow.php
@@ -3,6 +3,15 @@ $title = 'Control Flow';
 include "layout/header.php";
 ?>
 
+<h3> 1. Write a script that gets the current month and prints one of the following responses, 
+    depending on whether it's August or not: <br>
+It's August, so it's still holiday. <br>
+Not August, this is Month-name so I don't have any holidays </h3>
+<?php
+
+
+?>
+
 
 
 
